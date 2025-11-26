573095914bb367f51d336d38b1467cb5468f6213
Matleftod <97225801+Matleftod@users.noreply.github.com>
2025-11-26 20:45:01 +0100

    real section

--- site/snippets/sections/realisations.php
+++ site/snippets/sections/realisations.php
@@ -1,10 +1,6 @@
 <section id="realisations" class="py-24 bg-bg-1 text-text-1">
   <div class="max-w-[1200px] mx-auto justify-self-center">
 
-    <h2 class="text-4xl font-semibold text-text-3 mb-16 px-6">
-      Quelques réalisations
-    </h2>
-
     <!-- GRID -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 px-6">
 
@@ -21,20 +17,18 @@
                   hover:shadow-2xl transition-all duration-300">
 
         <!-- HEADER -->
-        <div class="flex items-center justify-between mb-6">
+        <div class="real-header mb-6">
+            <h3 class="text-xl font-semibold text-text-1 pl-3">
+                <?= $proj['name'] ?>
+            </h3>
 
-          <h3 class="text-xl font-semibold text-text-1">
-            <?= $proj['name'] ?>
-          </h3>
-
-          <div class="flex gap-1 text-accent-1">
-            <?php for ($i = 0; $i < 5; $i++): ?>
-              <svg viewBox="0 0 20 20" class="w-5 h-5 star fill-current text-accent-1">
-                <polygon points="10 1.5 12.8 7.4 19.3 7.8 14.2 12.1 15.7 18.5 10 15.2 4.3 18.5 5.8 12.1 .7 7.8 7.2 7.4"/>
-              </svg>
-            <?php endfor ?>
-          </div>
-
+            <div class="flex gap-1 text-accent-1 pr-3">
+                <?php for ($i = 0; $i < 5; $i++): ?>
+                <svg viewBox="0 0 20 20" class="w-5 h-5 star fill-current text-accent-1">
+                    <polygon points="10 1.5 12.8 7.4 19.3 7.8 14.2 12.1 15.7 18.5 10 15.2 4.3 18.5 5.8 12.1 .7 7.8 7.2 7.4"/>
+                </svg>
+                <?php endfor ?>
+            </div>
         </div>
 
         <!-- IMAGE -->
