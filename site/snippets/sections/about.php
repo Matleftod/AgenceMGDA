6a501197aded2f7746992721cf14945f48eb8ff7
Matleftod <97225801+Matleftod@users.noreply.github.com>
2025-12-01 20:18:12 +0100

    add feuille 2 -5 + new pot

--- site/snippets/sections/about.php
+++ site/snippets/sections/about.php
@@ -34,24 +34,92 @@
             />
         </svg>
         <img src="<?= url('assets/images/plante/feuille1.png') ?>"
-                class="feuille-img f1-img"
+                class="feuille-img f1-img animA"
                 alt="Feuille 1">
       </div>
 
 
       <!-- ============== FEUILLE 2–5 (visuelles, non interactives) ============== -->
 
-      <img src="<?= url('assets/images/plante/feuille2.png') ?>"
-           class="feuille f2" alt="Feuille 2">
+      <div class="feuille-wrapper f2-wrapper">
 
-      <img src="<?= url('assets/images/plante/feuille3.png') ?>"
-           class="feuille f3" alt="Feuille 3">
+        <svg 
+            class="feuille-hitbox f2-hitbox"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2974 3482"
+            preserveAspectRatio="xMidYMid meet"
+        >
+            <path
+            class="hitbox-path"
+            d="M1556,1778l-29-152-47-142-58-120-92-139-14-155-40-126-83-124-90-95L918,579,713,501,463,446l95,246,82,166,29,54,76,93,86,81,130,86,94,34,105,23,120,19,23-1,30,41,48,84,28,55,27,67,22,81,23,104,11,110,4,216,13-54,14-63,14-55Z"
+            fill="rgba(255,255,255,0.001)"
+            />
+        </svg>
 
-      <img src="<?= url('assets/images/plante/feuille4.png') ?>"
-           class="feuille f4" alt="Feuille 4">
+        <img src="<?= url('assets/images/plante/feuille2.png') ?>"
+            class="feuille-img f2-img animB"
+            alt="Feuille 2">
+    </div>
 
-      <img src="<?= url('assets/images/plante/feuille5.png') ?>"
-           class="feuille f5" alt="Feuille 5">
+      <div class="feuille-wrapper f3-wrapper">
+
+        <svg 
+            class="feuille-hitbox f3-hitbox"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2974 3482"
+            preserveAspectRatio="xMidYMid meet"
+        >
+            <path
+            class="hitbox-path"
+            d="M1560,1998l-14,23-19,48-11,37-7-44-14-39,20-104,23-87,18-57,34-90,31-73,36-70,21-36,1-28,28-123,30-79,20-38,73-64,137-85,37-18,103-32,168-13,162,10,138,19,77,22-87,110-57,72-138,140-46,37-77,50-80,44-49,22-105,20-106,3-85-7-93-23-29-8-8,9-13,24-27,64-27,70-17,56-20,74-17,81Z"
+            fill="rgba(255,255,255,0.001)"
+            />
+        </svg>
+
+        <img src="<?= url('assets/images/plante/feuille3.png') ?>"
+            class="feuille-img f3-img animA"
+            alt="Feuille 3">
+    </div>
+
+      <div class="feuille-wrapper f4-wrapper">
+
+        <svg 
+            class="feuille-hitbox f4-hitbox"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2974 3482"
+            preserveAspectRatio="xMidYMid meet"
+        >
+            <path
+            class="hitbox-path"
+            d="M1516,2105l-6,16-7,29-43-6-7-68-19-96-30-84-29-62-35-56-104,43-83,26h-45l-81-15-82-22-57-30-106-53-133-64L383,1561l-3-3,25-23,64-40,91-41,118-37,38-9,22-5,112-4,133,12,76,14,53,16,47,18,59,30,44,30,36,39,27,47,20,52,12,46,8,40,1,20,11,18,33,47,33,61,23,56,21,63,6,16Z"
+            fill="rgba(255,255,255,0.001)"
+            />
+        </svg>
+
+        <img src="<?= url('assets/images/plante/feuille4.png') ?>"
+            class="feuille-img f4-img animB"
+            alt="Feuille 4">
+    </div>
+
+      <div class="feuille-wrapper f5-wrapper">
+
+        <svg 
+            class="feuille-hitbox f5-hitbox"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2974 3482"
+            preserveAspectRatio="xMidYMid meet"
+        >
+            <path
+            class="hitbox-path"
+            d="M1570,2171l-67-21,10-37,13-41,21-54,15-20,70-94,60-42,74-47,109-47,75-17,90-7,46,3,57,11,57,18,56,30,55,43,63,66,36,45,43,68,50,98,19,54-332-28-68-6-140-25-53-16-77-43-60-50-39-52-46-75-13,1-18,16-16,20-32,51-22,47-18,50-10,44Z"
+            fill="rgba(255,255,255,0.001)"
+            />
+        </svg>
+
+        <img src="<?= url('assets/images/plante/feuille5.png') ?>"
+            class="feuille-img f5-img animA"
+            alt="Feuille 5">
+    </div>
 
     </div>
 
