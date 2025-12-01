1fb71cd7c29809dbc48df0c5447284406e70412a
Matleftod <97225801+Matleftod@users.noreply.github.com>
2025-12-01 17:36:35 +0100

    test svg

--- assets/js/home/about-eco.js
+++ assets/js/home/about-eco.js
@@ -1,3 +1,17 @@
 export function initAboutEco() {
-  
+    const wrappers = document.querySelectorAll('.feuille-wrapper');
+
+    wrappers.forEach(wrapper => {
+        const img  = wrapper.querySelector('.feuille-img');
+        const path = wrapper.querySelector('.hitbox-path');
+        if (!img || !path) return;
+
+        path.addEventListener('mouseenter', () => {
+        img.classList.add('is-hovered');
+        });
+
+        path.addEventListener('mouseleave', () => {
+        img.classList.remove('is-hovered');
+        });
+    });
 }
--- site/snippets/sections/about.php
+++ site/snippets/sections/about.php
@@ -12,18 +12,44 @@
         <img src="<?= url('assets/images/plante/pot.png') ?>" 
             class="plante-pot" alt="Pot">
 
-        <img src="<?= url('assets/images/plante/feuille1.png') ?>" 
-            class="feuille f1" alt="Feuille 1">
+        <div class="feuille-wrapper f1-wrapper">
 
+            <!-- Hitbox SVG invisible -->
+            <svg class="feuille-hitbox f1-hitbox"
+                width="800" height="1013"
+                viewBox="0 0 800 1013">
+                <path class="hitbox-path"
+                    d="M520 50
+                        C 560 120, 590 200, 580 300
+                        C 570 420, 500 510, 430 560
+                        C 380 600, 350 660, 355 710
+                        C 360 760, 380 820, 400 900
+                        C 380 880, 330 740, 325 680
+                        C 320 620, 350 550, 390 500
+                        C 450 430, 510 350, 520 260
+                        C 530 180, 510 110, 480 70
+                        Z"
+                    fill="white" />
+            </svg>
+
+            <img src="assets/images/plante/feuille1.png"
+                class="feuille-img f1-img"
+                alt="Feuille 1">
+        </div>
+
+        <div class="feuille-zone f2-zone"></div>
         <img src="<?= url('assets/images/plante/feuille2.png') ?>" 
             class="feuille f2" alt="Feuille 2">
 
+        <div class="feuille-zone f3-zone"></div>
         <img src="<?= url('assets/images/plante/feuille3.png') ?>" 
             class="feuille f3" alt="Feuille 3">
 
+        <div class="feuille-zone f4-zone"></div>
         <img src="<?= url('assets/images/plante/feuille4.png') ?>" 
             class="feuille f4" alt="Feuille 4">
 
+        <div class="feuille-zone f5-zone"></div>
         <img src="<?= url('assets/images/plante/feuille5.png') ?>" 
             class="feuille f5" alt="Feuille 5">
     </div>
