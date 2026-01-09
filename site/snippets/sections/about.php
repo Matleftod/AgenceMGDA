4c335a99f47d38603d2ab67dc0fd21f41d2cee3e
Matleftod <97225801+Matleftod@users.noreply.github.com>
2026-01-09 18:24:27 +0100

    fix src vide

--- site/snippets/sections/about.php
+++ site/snippets/sections/about.php
@@ -1,3 +1,8 @@
+<?php
+// 1×1 transparent gif, évite les requêtes inutiles provoquées par src=""
+$placeholder = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+?>
+
 <section id="about" class="about-eco">
 
   <div class="about-eco-container">
@@ -13,20 +18,22 @@
         <div class="plante-wrapper">
 
           <!-- Glow -->
-          <img 
-            loading="lazy" 
-            src="" 
-            data-src="<?= url('assets/images/plante/glow.webp') ?>" 
-            class="plante-glow plante-layer" 
+          <img
+            loading="lazy"
+            decoding="async"
+            src="<?= $placeholder ?>"
+            data-src="<?= url('assets/images/plante/glow.webp') ?>"
+            class="plante-glow plante-layer"
             alt="Glow"
           >
 
           <!-- Pot -->
-          <img 
-            loading="lazy" 
-            src="" 
-            data-src="<?= url('assets/images/plante/pot.avif') ?>" 
-            class="plante-pot plante-layer" 
+          <img
+            loading="lazy"
+            decoding="async"
+            src="<?= $placeholder ?>"
+            data-src="<?= url('assets/images/plante/pot.avif') ?>"
+            class="plante-pot plante-layer"
             alt="Pot"
           >
 
@@ -37,79 +44,80 @@
                 d="M1512,1566l13-175,42-228,46-149,307-76,177-196,81-252,16-197-5-243L1934,182,1767,295,1607,490l-90,255,81,252-59,158-28,109-36,206Z"
                 fill="rgba(255,255,255,0.001)" />
             </svg>
-            <img 
-              loading="lazy" 
-              src="" 
-              data-src="<?= url('assets/images/plante/feuille1.avif') ?>" 
-              class="feuille-img f1-img animA plante-layer" 
+            <img
+              loading="lazy"
+              decoding="async"
+              src="<?= $placeholder ?>"
+              data-src="<?= url('assets/images/plante/feuille1.avif') ?>"
+              class="feuille-img f1-img animA plante-layer"
               alt="Feuille 1"
             >
           </div>
 
           <!-- FEUILLE 2 -->
           <div class="feuille-wrapper f2-wrapper" data-leaf="2">
-            <svg class="feuille-hitbox f2-hitbox" xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 2974 3482">
+            <svg class="feuille-hitbox f2-hitbox" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2974 3482">
               <path class="hitbox-path"
                 d="M1556,1778l-29-152-47-142-58-120-92-139-14-155-40-126-83-124-90-95L918,579,713,501,463,446l95,246,82,166,29,54,76,93,86,81,130,86,94,34,105,23,120,19,23-1,30,41,48,84,28,55,27,67,22,81,23,104,11,110,4,216,13-54,14-63,14-55Z"
                 fill="rgba(255,255,255,0.001)" />
             </svg>
-            <img 
-              loading="lazy" 
-              src="" 
-              data-src="<?= url('assets/images/plante/feuille2.avif') ?>" 
-              class="feuille-img f2-img animB plante-layer" 
+            <img
+              loading="lazy"
+              decoding="async"
+              src="<?= $placeholder ?>"
+              data-src="<?= url('assets/images/plante/feuille2.avif') ?>"
+              class="feuille-img f2-img animB plante-layer"
               alt="Feuille 2"
             >
           </div>
 
           <!-- FEUILLE 3 -->
           <div class="feuille-wrapper f3-wrapper" data-leaf="3">
-            <svg class="feuille-hitbox f3-hitbox" xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 2974 3482">
+            <svg class="feuille-hitbox f3-hitbox" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2974 3482">
               <path class="hitbox-path"
                 d="M1560,1998l-14,23-19,48-11,37-7-44-14-39,20-104,23-87,18-57,34-90,31-73,36-70,21-36,1-28,28-123,30-79,20-38,73-64,137-85,37-18,103-32,168-13,162,10,138,19,77,22-87,110-57,72-138,140-46,37-77,50-80,44-49,22-105,20-106,3-85-7-93-23-29-8-8,9-13,24-27,64-27,70-17,56-20,74-17,81Z"
                 fill="rgba(255,255,255,0.001)" />
             </svg>
-            <img 
-              loading="lazy" 
-              src="" 
-              data-src="<?= url('assets/images/plante/feuille3.avif') ?>" 
-              class="feuille-img f3-img animA plante-layer" 
+            <img
+              loading="lazy"
+              decoding="async"
+              src="<?= $placeholder ?>"
+              data-src="<?= url('assets/images/plante/feuille3.avif') ?>"
+              class="feuille-img f3-img animA plante-layer"
               alt="Feuille 3"
             >
           </div>
 
           <!-- FEUILLE 4 -->
           <div class="feuille-wrapper f4-wrapper" data-leaf="4">
-            <svg class="feuille-hitbox f4-hitbox" xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 2974 3482">
+            <svg class="feuille-hitbox f4-hitbox" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2974 3482">
               <path class="hitbox-path"
                 d="M1516,2105l-6,16-7,29-43-6-7-68-19-96-30-84-29-62-35-56-104,43-83,26h-45l-81-15-82-22-57-30-106-53-133-64L383,1561l-3-3,25-23,64-40,91-41,118-37,38-9,22-5,112-4,133,12,76,14,53,16,47,18,59,30,44,30,36,39,27,47,20,52,12,46,8,40,1,20,11,18,33,47,33,61,23,56,21,63,6,16Z"
                 fill="rgba(255,255,255,0.001)" />
             </svg>
-            <img 
-              loading="lazy" 
-              src="" 
-              data-src="<?= url('assets/images/plante/feuille4.avif') ?>" 
-              class="feuille-img f4-img animB plante-layer" 
+            <img
+              loading="lazy"
+              decoding="async"
+              src="<?= $placeholder ?>"
+              data-src="<?= url('assets/images/plante/feuille4.avif') ?>"
+              class="feuille-img f4-img animB plante-layer"
               alt="Feuille 4"
             >
           </div>
 
           <!-- FEUILLE 5 -->
           <div class="feuille-wrapper f5-wrapper" data-leaf="5">
-            <svg class="feuille-hitbox f5-hitbox" xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 2974 3482">
+            <svg class="feuille-hitbox f5-hitbox" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2974 3482">
               <path class="hitbox-path"
                 d="M1570,2171l-67-21,10-37,13-41,21-54,15-20,70-94,60-42,74-47,109-47,75-17,90-7,46,3,57,11,57,18,56,30,55,43,63,66,36,45,43,68,50,98,19,54-332-28-68-6-140-25-53-16-77-43-60-50-39-52-46-75-13,1-18,16-16,20-32,51-22,47-18,50-10,44Z"
                 fill="rgba(255,255,255,0.001)" />
             </svg>
-            <img 
-              loading="lazy" 
-              src="" 
-              data-src="<?= url('assets/images/plante/feuille5.avif') ?>" 
-              class="feuille-img f5-img animC plante-layer" 
+            <img
+              loading="lazy"
+              decoding="async"
+              src="<?= $placeholder ?>"
+              data-src="<?= url('assets/images/plante/feuille5.avif') ?>"
+              class="feuille-img f5-img animC plante-layer"
               alt="Feuille 5"
             >
           </div>
